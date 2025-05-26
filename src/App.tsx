c899e3696b50cc9a4b42514bdfe46357913cbe7e
Mayssa <mayssafccn16@gmail.com>
2025-05-26 16:03:38 -0300

    fix: add basename to React Router and update manifest paths for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/aura-clinic-bliss">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
